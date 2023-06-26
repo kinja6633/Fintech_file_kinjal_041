b131a4008cbcdb35db803a2eabf1d7d81ba7803b
unknown <kinjaaa6633@gmail.com>
2023-06-25 20:20:23 -0700

    this is swap file from the seccind folder for do another commit

---
+++ swap_05.py
@@ -0,0 +1,16 @@
+# Python program to swap two variables
+a = str(input("Enter any character:"))
+b = str(input("Enter any character:"))
+print(a, b)
+
+# Basic way
+# temp = a
+# a = b
+# b = temp
+# print("Basic",a, b)
+
+# another way
+a,b = b,a
+print("eassy:",a,b)
+
+
